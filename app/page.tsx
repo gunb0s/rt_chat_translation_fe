da60bd8a9e65fd54ac85ed27f3d9c8eeeba1b2cb
cain <gunbos1031@gmail.com>
2024-05-29 20:58:51 +0900

    refactor: ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,25 @@
 import Head from 'next/head';
-import ProfileHeader from "@/app/components/ProfileHeader";
-import ChatList from "@/app/components/ChatList";
-import ChatWindow from "@/app/components/ChatWindow";
+import ProfileHeader from "@/components/ProfileHeader";
+import ChatList from "@/components/ChatList";
 
-const Home: React.FC = () => {
-  return (
-      <div className="flex h-screen">
-        <Head>
-          <title>Chat App</title>
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-        <div className="w-1/4 p-4 bg-gray-100">
-          <ProfileHeader />
-          <ChatList />
+const Home = () => {
+    return (
+        <div className="flex flex-col md:flex-row h-screen">
+            <Head>
+                <title>Chat App</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <div className="md:w-1/3 bg-gray-100 p-4">
+                <ProfileHeader />
+                <ChatList />
+            </div>
+            <div className="hidden md:block md:w-2/3 p-4 bg-white">
+                <div className="flex items-center justify-center h-full text-gray-500">
+                    Select a chat to start messaging
+                </div>
+            </div>
         </div>
-        <div className="w-3/4 p-4">
-          <ChatWindow />
-        </div>
-      </div>
-  );
+    );
 }
 
 export default Home;
--- components/ProfileHeader.tsx
+++ components/ProfileHeader.tsx
@@ -2,9 +2,16 @@ import Image from 'next/image';
 
 const ProfileHeader = () => (
     <div className="flex items-center p-4 bg-purple-200 rounded-lg">
-        <Image src={""} alt="Profile Picture" className="w-12 h-12 rounded-full" />
+        <Image
+            src="https://randomuser.me/api/portraits/men/75.jpg"
+            alt="Profile Picture"
+            width={48}
+            height={48}
+            className="w-12 h-12 rounded-full"
+            priority
+        />
         <div className="ml-4">
-            <h2 className="text-lg font-semibold">Pedrik Ronner</h2>
+            <h2 className="text-lg font-semibold text-black">Pedrik Ronner</h2>
             <p className="text-sm text-green-500">Active now</p>
         </div>
         <div className="ml-auto">
