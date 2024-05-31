6fda1da3609ef6b8e07189bcc7db24737ca750b3
cain <gunbos1031@gmail.com>
2024-05-31 10:16:02 +0900

    feat: request github access token to server

--- app/(auth)/github/complete/route.ts
+++ app/(auth)/github/complete/route.ts
@@ -1,12 +1,41 @@
-import {NextRequest} from "next/server";
-import {redirect} from "next/navigation";
+import { NextRequest } from "next/server";
+import { redirect } from "next/navigation";
 
 export async function GET(request: NextRequest) {
-    const code = request.nextUrl.searchParams.get("code");
-    if (!code) {
-        return new Response(null, {
-            status: 400,
-        });
-    }
-    return redirect("/home");
+  const code = request.nextUrl.searchParams.get("code");
+  if (!code) {
+    return new Response(null, {
+      status: 400,
+    });
+  }
+
+  const accessTokenParams = new URLSearchParams({
+    client_id: process.env.GITHUB_CLIENT_ID!,
+    client_secret: process.env.GITHUB_CLIENT_SECRET!,
+    code,
+  }).toString();
+
+  const signUpRequestUrl = `http://localhost:8080/auth/${code}`;
+  const response = await fetch(signUpRequestUrl, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+    },
+  });
+
+  const json = await response.json();
+
+  console.log(json);
+
+  /*
+    const accessTokenURL = `https://github.com/login/oauth/access_token?${accessTokenParams}`;
+    const accessTokenResponse = await fetch(accessTokenURL, {
+        method: "POST",
+        headers: {
+            Accept: "application/json",
+        },
+    });
+    */
+
+  return redirect("/home");
 }
