5f28e6911de0b1d8d2675f45e5f3d37e94d318c7
cain <gunbos1031@gmail.com>
2024-06-01 22:45:04 +0900

    feat: show login user name

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,25 +1,25 @@
-import Head from 'next/head';
+import Head from "next/head";
 import ProfileHeader from "@/components/ProfileHeader";
 import ChatList from "@/components/ChatList";
 
 const Home = () => {
-    return (
-        <div className="flex flex-col md:flex-row h-screen">
-            <Head>
-                <title>Chat App</title>
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-            <div className="md:w-1/3 bg-gray-100 p-4">
-                <ProfileHeader />
-                <ChatList />
-            </div>
-            <div className="hidden md:block md:w-2/3 p-4 bg-white">
-                <div className="flex items-center justify-center h-full text-gray-500">
-                    Select a chat to start messaging
-                </div>
-            </div>
+  return (
+    <div className="flex flex-col md:flex-row h-screen">
+      <Head>
+        <title>Chat App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="md:w-1/3 bg-gray-100 p-4">
+        <ProfileHeader />
+        <ChatList />
+      </div>
+      <div className="hidden md:block md:w-2/3 p-4 bg-white">
+        <div className="flex items-center justify-center h-full text-gray-500">
+          Select a chat to start messaging
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
 
 export default Home;
--- components/ProfileHeader.tsx
+++ components/ProfileHeader.tsx
@@ -1,27 +1,75 @@
-import Image from 'next/image';
+import Image from "next/image";
+import getSession from "@/lib/session";
+import { notFound, redirect } from "next/navigation";
 
-const ProfileHeader = () => (
-    <div className="flex items-center p-4 bg-purple-200 rounded-lg">
-        <Image
-            src="https://randomuser.me/api/portraits/men/75.jpg"
-            alt="Profile Picture"
-            width={48}
-            height={48}
-            className="w-12 h-12 rounded-full"
-            priority
-        />
-        <div className="ml-4">
-            <h2 className="text-lg font-semibold text-black">Pedrik Ronner</h2>
-            <p className="text-sm text-green-500">Active now</p>
-        </div>
-        <div className="ml-auto">
-            <button className="p-2 rounded-full bg-gray-200">
-                <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 4v16m8-8H4" />
-                </svg>
-            </button>
-        </div>
+async function getUser() {
+  const session = await getSession();
+  if (session.accessToken) {
+    const response = await (
+      await fetch("http://localhost:8080/user/me", {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${session.accessToken}`,
+        },
+      })
+    ).json();
+
+    if (response.data) {
+      return response.data;
+    }
+  }
+  notFound();
+}
+
+async function Username() {
+  //await new Promise((resolve) => setTimeout(resolve, 10000));
+  const user = await getUser();
+  return (
+    <div className="ml-4">
+      <h2 className="text-lg font-semibold text-black">{user?.username}</h2>
+      <p className="text-sm text-green-500">Active now</p>
     </div>
-);
+  );
+}
 
-export default ProfileHeader;
+export default function ProfileHeader() {
+  const logOut = async () => {
+    "use server";
+    const session = await getSession();
+    session.destroy();
+    redirect("/");
+  };
+
+  return (
+    <div className="flex items-center p-4 bg-purple-200 rounded-lg">
+      <Image
+        src="https://randomuser.me/api/portraits/men/75.jpg"
+        alt="Profile Picture"
+        width={48}
+        height={48}
+        className="w-12 h-12 rounded-full"
+        priority
+      />
+      <Username />
+      <div className="ml-auto">
+        <form action={logOut}>
+          <button className="p-2 rounded-full bg-gray-200">
+            <svg
+              className="w-6 h-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
